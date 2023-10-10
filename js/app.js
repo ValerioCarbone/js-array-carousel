5f8facf23e904b50f252c74654bca53c376247ce
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-10 16:52:10 +0200

    CHANGE-Js carousel

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@
 //         - Richiamiamo un elemento dall'html e al suo interno inseriamo le nostre immagini
 //         - A tutte le immagini tranne quella con la classe Display daremo la classe Hide 
 
-const imagesCarousel = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp' ]
+const imagesCarousel = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp']
 
 let containerDOMElement = document.querySelector('.container')
 let imgDOMElement = document.createElement('img')
 console.log(imagesCarousel.length)
 
-for (let i = 0; i <= imagesCarousel.length - 1; i++){
-
-    imgDOMElement.outerHTML = (`<img src=${imagesCarousel[i]} alt="">`) 
+for (let i = 0; i <= imagesCarousel.length; i++){
+    
+    imgDOMElement.outerHTML = (`<img src=${imagesCarousel[i - 1]} alt="">`) 
     containerDOMElement.append(imgDOMElement)
 }
