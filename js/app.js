18c2a5efe5ad40eaa74c31ba901d828288f7288a
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-11 10:31:13 +0200

    ADD-Thumbnails carousel active

--- js/app.js
+++ js/app.js
@@ -4,16 +4,19 @@
 //         - Creiamo un array che contenga tutte le immagini(url)
 //         - Creiamo un ciclo for che tramite il template literal mostri ciclicamente tutte le immagini
 //         - Richiamiamo un elemento dall'html e al suo interno inseriamo le nostre immagini
-//         - A tutte le immagini tranne quella con la classe Display daremo la classe Hide 
+//         - A tutte le immagini diamo la classe hidden
 
 const imagesCarousel = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp']
 
 let boxDOMElement = document.querySelector('.box')
 
+let thumbnailsDOMElement = document.querySelector('.thumbnails')
+
+let currentImg 
 
 for (let i = 0; i < imagesCarousel.length; i++) {
 
-    const currentImg = imagesCarousel[i]
+    currentImg = imagesCarousel[i]
     const htmlImgString = ` 
     <div class = 'hidden'>
         <img src=${currentImg}>
@@ -21,27 +24,47 @@ for (let i = 0; i < imagesCarousel.length; i++) {
     `
     boxDOMElement.innerHTML += htmlImgString
 
+    const htmlThumbnailsString = ` 
+    <div class = 'not-active'>
+        <img src=${currentImg}>
+    </div>
+    `
+    thumbnailsDOMElement.innerHTML += htmlThumbnailsString
 }
 
 const imgDOMElements = document.getElementsByClassName('hidden')
+
+const thumbnailsDOMElements = document.getElementsByClassName('not-active')
 
 let number = 0
 
 let imgDisplayDOMElement = imgDOMElements[number]
 
+let thumbnailsDisplayDOMElement = thumbnailsDOMElements[number]
+
 imgDisplayDOMElement.classList.replace('hidden', 'display')
+
+thumbnailsDisplayDOMElement.classList.replace('not-active', 'active')
 
 const imgUpDOMElement = document.getElementById('arrow-up')
 
 imgUpDOMElement.addEventListener('click', function() {
 
+    thumbnailsDisplayDOMElement.classList.replace('active', 'not-active')
+    
     imgDisplayDOMElement.classList.replace('display', 'hidden')
+    
     number += 1
-    if (number > 3){
+    if (number > 4){
         number = 0
     }
     imgDisplayDOMElement = imgDOMElements[number]
+
+    thumbnailsDisplayDOMElement = thumbnailsDOMElements[number]
+    
     imgDisplayDOMElement.classList.replace('hidden', 'display')
+
+    thumbnailsDisplayDOMElement.classList.replace('not-active', 'active')
 }
 )
 
@@ -49,55 +72,22 @@ const imgDownDOMElement = document.getElementById('arrow-down')
 
 imgDownDOMElement.addEventListener('click', function() {
 
+    thumbnailsDisplayDOMElement.classList.replace('active', 'not-active')
+
     imgDisplayDOMElement.classList.replace('display', 'hidden')
+    
     number -= 1
     if (number < 0){
         number = 4
     }
     imgDisplayDOMElement = imgDOMElements[number]
+    
+    thumbnailsDisplayDOMElement = thumbnailsDOMElements[number]
+
     imgDisplayDOMElement.classList.replace('hidden', 'display')
+
+    thumbnailsDisplayDOMElement.classList.replace('not-active', 'active')
 }
 )
 
-
-// imgUpDOMElement.addEventListener('click', function() {
-
-//     imgDisplayDOMElement.classList.remove('display')
-
-//     number += 1
-//     if (number > 3){
-//     number = 0
-//     }
-
-//     imgDisplayDOMElement.classList.add('display')
-// }
-// )
-
-// const imgDownDOMElement = document.getElementById('arrow-down')
-
-// imgDownDOMElement.addEventListener('click', function() {
-
-//     imgDisplayDOMElement.classList.replace('display', 'hidden')
-
-//     number -= 1
-//     if (number < 0){
-//     number = 3
-//     }
-
-//     imgDisplayDOMElement.classList.replace('hidden', 'display')
-// }
-// )
-
-// // imgDownDOMElement.addEventListener('click', function(){
-
-// //     number -= 1
-// //     if (number < 0){
-// //         number = 3
-// //     }
-// //     console.log(imgDOMElements[number])
-// //     let imgDisplayDOMElement = imgDOMElements[number]
-
-// //     imgDisplayDOMElement.classList.add('display')
-// // }
-// // )
 
