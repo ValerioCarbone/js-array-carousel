8ac860f5cf1b7e49e76e766c64d73b67d624ba3b
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-11 10:37:03 +0200

    ADD-Pseudo codice

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@
 //     - Creiamo un markup statico con un container contenente una foto
 //     - Creiamo lo stesso layout tramite js
 //         - Creiamo un array che contenga tutte le immagini(url)
-//         - Creiamo un ciclo for che tramite il template literal mostri ciclicamente tutte le immagini
+//         - Creiamo un ciclo for che tramite il template literal che crei degli elementi nell'html con una classe
 //         - Richiamiamo un elemento dall'html e al suo interno inseriamo le nostre immagini
-//         - A tutte le immagini diamo la classe hidden
 
 const imagesCarousel = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp']
 
@@ -32,9 +31,13 @@ for (let i = 0; i < imagesCarousel.length; i++) {
     thumbnailsDOMElement.innerHTML += htmlThumbnailsString
 }
 
+// Richiamare gli elementi per classe
+
 const imgDOMElements = document.getElementsByClassName('hidden')
 
 const thumbnailsDOMElements = document.getElementsByClassName('not-active')
+
+// Dare la classe display e active all'elemento che voglio sia visibile e contornato
 
 let number = 0
 
@@ -45,6 +48,12 @@ let thumbnailsDisplayDOMElement = thumbnailsDOMElements[number]
 imgDisplayDOMElement.classList.replace('hidden', 'display')
 
 thumbnailsDisplayDOMElement.classList.replace('not-active', 'active')
+
+// Richiamare le freccette presenti nell'html
+// Richiamare l'evento click per ogni freccetta
+// Ad ogni click rimuovere le classi display e active dalla precedente img
+// Ad ogni click cambiare l'elemento che ha la classe display e active 
+
 
 const imgUpDOMElement = document.getElementById('arrow-up')
 
